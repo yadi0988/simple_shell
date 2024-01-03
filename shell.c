9923d29931146b24155973a4496c3b4aff8383a0
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2024-01-03 23:35:12 +0100

    updates

--- shell.c
+++ shell.c
@@ -1,22 +1,34 @@
 #include "shell.h"
+extern char **environ;
+
 /**
- *
- **/
+ * main - function
+ * argc: number of argument
+ * argv: argv
+ * Return: int
+**/
 
-int main(__attribute__((unused))int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  char *arg[] = {NULL, NULL};
+  char **env;
+  char **arg;
   char *envp[] = {NULL};
-  char *buffer = NULL; 
+  char *buffer = NULL;
   size_t size = 0;
-  ssize_t read;
   int pid, status;
+  bool is_interactive;
 
+  if (argc > 1)
+    {
+      printf("%s: 0: Can't open %s\n", argv[0], argv[1]);
+      exit(-1);
+    }
+  is_interactive = isatty(fileno(stdin));
   while (1)
     {
+      if (is_interactive)
       printf("#cisfun$ ");
-      read = getline(&buffer, &size, stdin);
-      if (read == -1)
+      if (getline(&buffer, &size, stdin) == -1)
 	{
 	  if (feof(stdin))
 	    {
@@ -30,36 +42,65 @@ int main(__attribute__((unused))int argc, char *argv[])
 	    }
 	}
       buffer[strlen(buffer) - 1] = '\0';
+      
+      arg = get_arg(buffer);
+      buffer = NULL;
 
-      pid = fork();
+      if (!(strcmp(arg[0], "exit")) && arg[1] == NULL)
+	{
+	  _free(&arg);
+	  break;
+	}
+      else if (!(strcmp(arg[0], "exit")) && arg[1] != NULL)
+	{
+	  _free(&arg);
+	  printf("%s: not accept argument.\n", argv[0]);
+	  continue;
+	}
+
+      if (!(strcmp(arg[0], "env")) && arg[1] == NULL)
+	{
+	  for (env = environ; *env != NULL; env++)
+	    printf("%s\n", *env);
+	  continue;
+	}
+      else if (!(strcmp(arg[0], "env")) && arg[1] != NULL)
+	{
+	  _free(&arg);
+	  continue;
+	}
+      
+      if ((access(arg[0], X_OK)))
+	{
+	  arg[0] = get_path(_strcat("/", arg[0]));
+	  if (arg[0] == NULL)
+	    {
+	      printf("%s: No such file or direcyory\n", argv[0]);
+	      continue;
+	    }
+	}
+       pid = fork();
 
       if (pid < 0)
 	{
 	  perror("fork gets error!!!\n");
+	  _free(&arg);
 	  return (-1);
 	}
       else if (pid == 0)
 	{
-	  
-	  arg[0] = malloc((strlen(buffer) + 1) * sizeof(char));
-	  if (arg[0] == NULL)
-	    return (-1);
-
-	  strcpy(arg[0], buffer);
-	  
-	  if (execve(buffer, arg, envp) == -1)
+	  if (execve(arg[0], arg, envp) == -1)
 	    {
 	      printf("%s: No such file or direcyory\n", argv[0]);
-	      free(buffer);
-	      free(arg[0]);
+	      _free(&arg);
 	      return (-1);
 	    }
 	}
       else
 	{
 	  wait(&status);
+	  _free(&arg);
 	}
     }
-
   return (0);
 }
