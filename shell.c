3b319e1471e200f875dc258905f686c34476d355
Yadamzer Terefe <terefeyadamzer@gmail.com>
2024-01-04 23:56:44 +0100

    up-d

--- shell.c
+++ shell.c
@@ -2,107 +2,127 @@
 extern char **environ;
 
 /**
- * main - function
- * argc: number of argument
- * argv: argv
- * Return: int
-**/
+ *
+ **/
+
+void display_prompt()
+  {
+    if (isatty(fileno(stdin)))
+      printf("#cisfun$ ");
+  }
+
+void execute_command(char **arg, char **envp, char *arg0)
+  {
+    int pid, status;
+
+    pid = fork();
+
+    if (pid < 0)
+      {
+	perror("fork gets error!!!\n");
+	_free(&arg);
+	exit(-1);
+      }
+    else if (pid == 0)
+      {
+	if (execve(arg[0], arg, envp) == -1)
+	  {
+	    printf("%s: No such file or directory\n", arg0);
+	    _free(&arg);
+	    exit(-1);
+	  }
+      }
+    else
+      {
+	wait(&status);
+	_free(&arg);
+      }
+  }
+
+void handle_exit(char **arg, char *argv[])
+  {
+    if (!(strcmp(arg[0], "exit")) && arg[1] == NULL)
+      {
+	_free(&arg);
+	exit(0);
+      }
+    else if (!(strcmp(arg[0], "exit")) && arg[1] != NULL)
+      {
+	_free(&arg);
+	printf("%s: not accept argument.\n", argv[0]);
+      }
+  }
+
+void handle_env(char **arg)
+  {
+    char **env;
+
+    if (!(strcmp(arg[0], "env")) && arg[1] == NULL)
+      {
+	for (env = environ; *env != NULL; env++)
+	  printf("%s\n", *env);
+      }
+    else if (!(strcmp(arg[0], "env")) && arg[1] != NULL)
+      {
+	_free(&arg);
+      }
+  }
 
 int main(int argc, char *argv[])
-{
-  char **env;
-  char **arg;
-  char *envp[] = {NULL};
-  char *buffer = NULL;
-  size_t size = 0;
-  int pid, status, read;
-  bool is_interactive;
+  {
+    char *env[] = {NULL};
+    char **arg;
+    char *buffer = NULL;
+    size_t size = 0;
+    int read;
 
-  if (argc > 1)
-    {
-      printf("%s: 0: Can't open %s\n", argv[0], argv[1]);
-      exit(-1);
-    }
-  is_interactive = isatty(fileno(stdin));
-  while (1)
-    {
-      if (is_interactive)
-      printf("#cisfun$ ");
-      read = my_getline(&buffer, &size, stdin);
-      if (read == -1)
-	{
-	  if (feof(stdin))
-	    {
-	      printf("\n");
-	      break;
-	    }
-	  else
-	    {
-	      perror("getline fail!!!\n");
-	      return (-1);
-	    }
-	}
-      else if (read == -2)
-	continue;
+    if (argc > 1)
+      {
+	printf("%s: 0: Can't open %s\n", argv[0], argv[1]);
+	exit(-1);
+      }
 
-      arg = get_arg(buffer);
-      buffer = NULL;
+    while (1)
+      {
+	display_prompt();
+	read = my_getline(&buffer, &size, stdin);
 
-      if (!(strcmp(arg[0], "exit")) && arg[1] == NULL)
-	{
-	  _free(&arg);
-	  break;
-	}
-      else if (!(strcmp(arg[0], "exit")) && arg[1] != NULL)
-	{
-	  _free(&arg);
-	  printf("%s: not accept argument.\n", argv[0]);
-	  continue;
-	}
+	if (read == -1)
+	  {
+	    if (feof(stdin))
+	      {
+		printf("\n");
+		break;
+	      }
+	    else
+	      {
+		perror("getline fail!!!\n");
+		return (-1);
+	      }
+	  }
+	else if (read == -2)
+	  {
+	    continue;
+	  }
 
-      if (!(strcmp(arg[0], "env")) && arg[1] == NULL)
-	{
-	  for (env = environ; *env != NULL; env++)
-	    printf("%s\n", *env);
-	  continue;
-	}
-      else if (!(strcmp(arg[0], "env")) && arg[1] != NULL)
-	{
-	  _free(&arg);
-	  continue;
-	}
-      
-      if ((access(arg[0], X_OK)))
-	{
-	  arg[0] = get_path(_strcat("/", arg[0]));
-	  if (arg[0] == NULL)
-	    {
-	      printf("%s: No such file or direcyory\n", argv[0]);
-	      continue;
-	    }
-	}
-       pid = fork();
+	arg = get_arg(buffer);
+	buffer = NULL;
 
-      if (pid < 0)
-	{
-	  perror("fork gets error!!!\n");
-	  _free(&arg);
-	  return (-1);
-	}
-      else if (pid == 0)
-	{
-	  if (execve(arg[0], arg, envp) == -1)
-	    {
-	      printf("%s: No such file or direcyory\n", argv[0]);
-	      _free(&arg);
-	      return (-1);
-	    }
-	}
-      else
-	{
-	  wait(&status);
-	  _free(&arg);
-	}
-    }
-  return (0);
-}
+	handle_exit(arg, argv);
+	handle_env(arg);
+
+	if (access(arg[0], X_OK))
+	  {
+	    arg[0] = get_path(_strcat("/", arg[0]));
+	    if (arg[0] == NULL)
+	      {
+		printf("%s: NO such file or directory\n", argv[0]);
+		continue;
+	      }
+	  }
+
+	execute_command(arg, env, argv[0]);
+      }
+
+    return (0);
+  }
