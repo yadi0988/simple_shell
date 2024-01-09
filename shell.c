d6d2dd9e3208192cf8f34fb810ef825e00f76f48
Yadamzer Terefe <terefeyadamzer@gmail.com>
2024-01-09 23:30:31 +0100

    update

--- shell.c
+++ shell.c
@@ -1,131 +1,44 @@
 #include "shell.h"
-extern char **environ;
 
 /**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- **/
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-void display_prompt()
-  {
-    if (isatty(fileno(stdin)))
-      printf("#cisfun$ ");
-  }
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-void execute_command(char **arg, char **envp, char *arg0)
-  {
-    int pid, status;
-
-    pid = fork();
-
-    if (pid < 0)
-      {
-	perror("fork gets error!!!\n");
-	_free(&arg);
-	exit(-1);
-      }
-    else if (pid == 0)
-      {
-	if (execve(arg[0], arg, envp) == -1)
-	  {
-	    printf("%s: No such file or directory\n", arg0);
-	    _free(&arg);
-	    exit(-1);
-	  }
-      }
-    else
-      {
-	wait(&status);
-	_free(&arg);
-      }
-  }
-
-void handle_exit(char **arg)
-  {
-    int status;
-    
-    if (!(strcmp(arg[0], "exit")) && arg[1] == NULL)
-      {
-	_free(&arg);
-	exit(0);
-      }
-    else if (!(strcmp(arg[0], "exit")) && arg[1] != NULL)
-      {
-	status = atoi(arg[1]);
-	_free(&arg);
-	exit(status);
-      }
-  }
-
-void handle_env(char **arg)
-  {
-    char **env;
-
-    if (!(strcmp(arg[0], "env")) && arg[1] == NULL)
-      {
-	for (env = environ; *env != NULL; env++)
-	  printf("%s\n", *env);
-      }
-    else if (!(strcmp(arg[0], "env")) && arg[1] != NULL)
-      {
-	_free(&arg);
-      }
-  }
-
-int main(int argc, char *argv[])
-  {
-    char *env[] = {NULL};
-    char **arg;
-    char *buffer = NULL;
-    size_t size = 0;
-    int read;
-
-    if (argc > 1)
-      {
-	printf("%s: 0: Can't open %s\n", argv[0], argv[1]);
-	exit(-1);
-      }
-
-    while (1)
-      {
-	display_prompt();
-	read = my_getline(&buffer, &size, stdin);
-
-	if (read == -1)
-	  {
-	    if (feof(stdin))
-	      {
-		printf("\n");
-		break;
-	      }
-	    else
-	      {
-		perror("getline fail!!!\n");
-		return (-1);
-	      }
-	  }
-	else if (read == -2)
-	  {
-	    continue;
-	  }
-
-	arg = get_arg(buffer);
-	buffer = NULL;
-
-	handle_exit(arg);
-	handle_env(arg);
-
-	if (access(arg[0], X_OK))
-	  {
-	    arg[0] = get_path(_strcat("/", arg[0]));
-	    if (arg[0] == NULL)
-	      {
-		printf("%s: NO such file or directory\n", argv[0]);
-		continue;
-	      }
-	  }
-
-	execute_command(arg, env, argv[0]);
-      }
-
-    return (0);
-  }
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
